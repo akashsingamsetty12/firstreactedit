81b674f144416af819d310a21beec8e7f4d349b8
akashsingamsetty12 <akashsingamsetty123@gmail.com>
2024-09-25 15:26:40 +0530

    adding

---
+++ firstreact/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+    <GetComponent/>
+    <GetComponent/>
+    </div>
+  );
+}
+const GetComponent =()=>{
+  return(
+    <div class="App">
+      <h1>Akash</h1>
+      <h1>18</h1>
+      <h1>@gmail.com</h1>
+
+    </div>
+  );
+};
+
+export default App;
