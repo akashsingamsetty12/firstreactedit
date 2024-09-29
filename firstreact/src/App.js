105194e7e96b2e0f3929d554dd879e9aef421493
akashsingamsetty12 <akashsingamsetty123@gmail.com>
2024-09-29 22:56:58 +0530

    changes

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,23 +1,17 @@
 import "./App.css";
-function App() {
-  return (
-    <div>
-    <GetComponent name="akash" age={12} gmail="akashsingad@gmail.com" />
-    <GetComponent name="ak" age={12} gmail="akashsingad@gmail.com" />
-    <GetComponent/>
-    </div>
-  );
-}
-const GetComponent =(props)=>{
-  return(
-    <div class="App">
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.gmail}</h1>
-
-
-    </div>
-  );
+import { useState } from "react";
+function App(){
+const[age, setAge]=useState(0);
+const increase=()=>{
+  setAge(age+1);
+  console.log(age);
 };
 
+return(
+  <div>
+    {age}
+    <button onClick={increase}>Increase age</button>
+  </div>
+);
+}
 export default App;
