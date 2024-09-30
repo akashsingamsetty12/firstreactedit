7f6c96cd070fb41cce39eb11a65ff35db897c88c
akashsingamsetty12 <akashsingamsetty123@gmail.com>
2024-09-30 22:14:56 +0530

    changes

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,13 +1,26 @@
 import "./App.css";
 import { useState } from "react";
 function App(){
-const [show,setshow]=useState("blue");
+  const [todo,settodo]=useState([]);
+  const [newtask,setnewtask]=useState("");
+  const handle=(props)=>{
+    setnewtask(props.target.value);
+  };
+  const addtask=()=>{
+    const n=[todo,newtask];
+    settodo(n);
+    
 
-return(
-  <div>
-    <button onClick={()=>{setshow(show==="blue"?"red":"blue")}}>Show/Hide</button>
-<p style={{color:show}}>Hi my name is akash</p>
-</div>
-);
+  };
+  return(
+  <div className="App">
+    <div classname="First">
+      <input type="text" onChange={handle}/>
+      <button onClick={addtask}>add task</button>
+    </div>
+    <div classname="second">{todo}</div>
+  </div>
+  );
+
 }
 export default App;
